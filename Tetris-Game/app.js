2af065ccc57986c947dbb5e4ffcd5b9d545f3c69
AudhyBrilliantPratama <audhybrilliant@gmail.com>
2022-02-05 15:34:18 +0700

    added new blocks shape and more config

--- Tetris-Game/app.js
+++ Tetris-Game/app.js
@@ -5,5 +5,47 @@ document.addEventListener('DOMContentLoaded',() => { //untuk memberikan konfigur
     const StartBtn = document.querySelector('#start-button'); // untuk membuat variabel pada div id start button
     const width = 10;
 
-    console.log(squares);
-})
+    // Bagian Balok/Tetrominoes
+    const lTetromino = [ // Balok Berbentuk L
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+
+    const zTetromino = [ // Balok Berbentuk Z
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ];
+    
+    const tTetromino = [ // Balok Berbentuk T
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ];
+    
+    const oTetromino = [ // Balok Berbentuk Kotak/O
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ];
+    
+    const iTetromino = [ // Balok Berbentuk Panjang/I
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ];
+
+    const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]; // Pengelompokan Balok berdasarkan array
+    
+    let currentPosition = 4; // kondisi saat ini
+    let current = theTetrominoes [0]; // Menggambarkan kondisi awal balok
+
+    //buat poisi awal setelah balok dirotasi
+    
+});
