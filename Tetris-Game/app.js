cfe8e70873710f87777bbad6fa3221bbf90f9d3c
AudhyBrilliantPratama <audhybrilliant@gmail.com>
2022-02-05 14:48:48 +0700

    first commit

---
+++ Tetris-Game/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelector('#score')
+    const width = 10;
+
+    console.log(squares);
+})
