9749d2a0910b8efd586d4647c79e85b4867b7948
AudhyBrilliantPratama <audhybrilliant@gmail.com>
2022-02-05 15:11:26 +0700

    added new config and comments

--- Tetris-Game/app.js
+++ Tetris-Game/app.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector('.grid');
-    let squares = Array.from(document.querySelectorAll('.grid div'));
-    const ScoreDisplay = document.querySelector('#score')
+document.addEventListener('DOMContentLoaded',() => { //untuk memberikan konfigurasi pada file
+    const grid = document.querySelector('.grid'); // untuk membuat variabel pada div grid
+    let squares = Array.from(document.querySelectorAll('.grid div')); // untuk membuat array pada setiap div
+    const ScoreDisplay = document.querySelector('#score'); // untuk membuat variabel score pada div id score
+    const StartBtn = document.querySelector('#start-button'); // untuk membuat variabel pada div id start button
     const width = 10;
 
     console.log(squares);
